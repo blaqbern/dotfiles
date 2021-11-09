2b2e5b5580e2dd9fdf60c38bdbf4b698a9b5fc58
Chris Blackburn <cblackburn@launchdarkly.com>
2021-11-09 10:35:21 -0500

    switch to amaro; other small tweaks

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,4 @@
 alias ls='ls -la'
-alias rm='rm -i'
-alias g='git'
 alias vim='nvim'
 alias viml='nvim -c "colo forgotten-light" -c "hi ColorColumn guibg=#f0f5ff" -c "hi NonText guifg=#ffffff"'
 alias dump_nm='rm -rf node_modules && yarn'
--- zsh/zsh_init.zsh
+++ zsh/zsh_init.zsh
@@ -3,3 +3,17 @@ autoload colors && colors
 autoload -Uz compinit && compinit
 bindkey -v
 export EDITOR=vim
+
+function create_color_funcs () {
+  local colors=( red green blue cyan magenta yellow )
+
+  [[ ! -d ~/zshfn ]] && mkdir ~/zshfn
+
+  for c in $colors; do
+    if [[ ! -f ~/zshfn/$c ]]; then
+      echo "echo %F{$c}\$1%f" > ~/zshfn/$c
+    fi
+  done
+}
+
+create_color_funcs
