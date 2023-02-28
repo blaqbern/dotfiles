2638ca398217065bb2f99ce7ebdccf3888634265
Chris Blackburn <cblackburn@launchdarkly.com>
2023-02-28 12:01:43 -0500

    Go updates and some other rando updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,4 @@
 alias ls='ls -la'
 alias vim='nvim'
-alias viml='nvim -c "colo forgotten-light" -c "hi ColorColumn guibg=#f0f5ff" -c "hi NonText guifg=#ffffff"'
+alias viml='nvim -c "colo amaro-light" -c "hi ColorColumn guibg=#eadfdf" -c "hi NonText guifg=#ffffff"'
 alias dump_nm='rm -rf node_modules && yarn'
